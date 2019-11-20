421e9967932368d3fc30400d08983d26f992c4a4
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2019-11-19 19:12:37 -0800

    index is rendering to localhost:3005 and can submit a post request

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,13 @@
+// $(document).ready();
 
+// variable to capture input field data
+// var $jsondata = $("#json-input").val();
 
+// click operation to capture the json data from input field
+// $("#submit").on(click, function() {
+//  console.log($jsondata);
+// });
+
+// post request to server with the json data
+
+// constatntly running get request to append converted CSV to page?
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+module.exports.app = app;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/upload_json', function(req, res) {
+  res.status(200).send('YES');
+})
+
+// app.get('/', (request, response) => {
+
+// })
+
+// Serve the client files
+app.use(express.static(__dirname + '/client'));
+
+var PORT = 3005;
+app.listen(PORT, () => console.log(`app is listening on port: ${PORT}`));

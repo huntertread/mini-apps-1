8bc0a133af0a6396c8f2ddafcf50a28b3908d70a
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2019-11-20 11:47:05 -0800

    initial directory and file set up + babel config. Confirmed that babel is compiling

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,21 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      checkout: []
+    }
+  }
+
+  render() {
+    return (
+      console.log('base html goes here');
+    )
+  }
+}
+
+export default App;
 
 
